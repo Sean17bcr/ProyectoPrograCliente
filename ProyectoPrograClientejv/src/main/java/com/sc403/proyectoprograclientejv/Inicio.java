5d653ba22bbf0e13f305ab25a345e209e6ccaa11
Sean17bcr <seanbcr123@gmail.com>
2022-08-07 16:06:17 -0600

    Create Inicio.java

---
+++ ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Inicio.java
@@ -0,0 +1,12 @@
+package com.sc403.proyectoprograclientejv;
+public class Inicio {
+    public static void main(String[] args) {
+        //asdlfkhjaklsd;f
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
