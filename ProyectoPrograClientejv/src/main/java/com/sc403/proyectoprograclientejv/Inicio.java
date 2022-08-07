a164ffa2f80c050ba93cf805bfda1d4d3d9ed145
Kendall-22 <ken.araya.solis@gmail.com>
2022-08-07 16:07:41 -0600

    Update Inicio.java
    Hola

--- ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Inicio.java
+++ ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Inicio.java
@@ -1,12 +1,7 @@
 package com.sc403.proyectoprograclientejv;
 public class Inicio {
     public static void main(String[] args) {
-        //asdlfkhjaklsd;f
-        
-        
-        
-        
-        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
