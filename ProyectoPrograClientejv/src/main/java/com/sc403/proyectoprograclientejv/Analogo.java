fddf456103fabf90d1a30adb634dedce80304662
Kendall-22 <ken.araya.solis@gmail.com>
2022-08-11 21:28:13 -0600

    Adding information to class Analogo

--- ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Analogo.java
+++ ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Analogo.java
@@ -1,8 +1,42 @@
 
 package com.sc403.proyectoprograclientejv;
 
+import javax.swing.JOptionPane;
+
 
 public class Analogo extends Cable {
     private String calidadVideo;
+
+    public Analogo(String calidadVideo, int Canales, String CanalExclusivo, double precio, String tipo, String Instalacion) {
+        super(Canales, CanalExclusivo, precio, tipo, Instalacion);
+        this.calidadVideo = calidadVideo;
+    }
+    
+    public Analogo() {
+        
+        this.calidadVideo = "";
+    }
+
+    public String getCalidadVideo() {
+        return calidadVideo;
+    }
+
+    public void setCalidadVideo(String calidadVideo) {
+        this.calidadVideo = calidadVideo;
+    }
+
+    @Override
+    public void pedir_datos() {
+        this.calidadVideo = JOptionPane.showInputDialog(null, "Ingrese la "
+                + "calidad de video del cliente: ");
+    }
+
+    @Override
+    public void imprimir_datos() {
+        JOptionPane.showMessageDialog(null, "La calidad de video: "
+                + " "+ this.calidadVideo );
+    }
+    
+    
     
 }
--- ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Fibra.java
+++ ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Fibra.java
@@ -8,19 +8,48 @@ public class Fibra extends Internet{
     private String simetrico;
     private String ping;
 
-    @Override
-    public void pedir_datos() {
-        super.pedir_datos(); 
-        this.simetrico = JOptionPane.showMessageDialog(null, "");
+    public Fibra(String simetrico, String ping, int Megas, String Router, double precio, String tipo, String Instalacion) {
+        super(Megas, Router, precio, tipo, Instalacion);
+        this.simetrico = simetrico;
+        this.ping = ping;
     }
     
+    public Fibra() { 
+        this.simetrico = "";
+        this.ping = "";
+    }
+
+    public String getSimetrico() {
+        return simetrico;
+    }
+
+    public void setSimetrico(String simetrico) {
+        this.simetrico = simetrico;
+    }
+
+    public String getPing() {
+        return ping;
+    }
+
+    public void setPing(String ping) {
+        this.ping = ping;
+    }
+    
+    
+    
+
+    @Override
+    public void pedir_datos() {
+        this.ping = JOptionPane.showInputDialog(null, "El ping por la ubicacion va a ser de: ");
+        this.simetrico = JOptionPane.showInputDialog(null, "Ingrese : ");
+    }
     
     
     
     @Override
     public void imprimir_datos() {
-        JOptionPane.showMessageDialog(null, "El tipo de servicio solicitado es:"
-                + " "+ this.tipo + "\n" 
+        JOptionPane.showMessageDialog(null, "Ping: "
+                + " "+ this.ping + "\n" 
                 + "El lugar de la instalacion es: " +this.Instalacion+ "\n"
                 + "Router: "+this.Router+ "\n"
                 + "La cantidad de megas solicitadas son: "+ this.Megas);
