34fd933d64b903b06c9f929122d6896a35c968c4
Kendall-22 <ken.araya.solis@gmail.com>
2022-08-12 15:36:13 -0600

    I finished with the classes Archivos_Plan, Plan

--- ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Archivos_Plan.java
+++ ProyectoPrograClientejv/src/main/java/com/sc403/proyectoprograclientejv/Archivos_Plan.java
@@ -23,14 +23,14 @@ public class Archivos_Plan {
 
     public Archivos_Plan() {
         this.ruta = "";
-        this.nombre_archivo = "Cliente.txt";
+        this.nombre_archivo = "Plan.txt";
         this.obj_Plan = new Plan();
     }
 
-    public Archivos_Plan(Cliente obj_Cliente) {
+    public Archivos_Plan(Plan obj_Plan) {
         this.ruta = "";
-        this.nombre_archivo = "Cliente.txt";
-        this.obj_Cliente = obj_Cliente;
+        this.nombre_archivo = "Plan.txt";
+        this.obj_Plan = obj_Plan;
     }
 
     public String getRuta() {
@@ -49,12 +49,12 @@ public class Archivos_Plan {
         this.nombre_archivo = nombre_archivo;
     }
 
-    public Cliente getObj_Cliente() {
-        return obj_Cliente;
+    public Plan getobj_Plan() {
+        return obj_Plan;
     }
 
-    public void setObj_Cliente(Cliente obj_Cliente) {
-        this.obj_Cliente = obj_Cliente;
+    public void setobj_Plan(Plan obj_Plan) {
+        this.obj_Plan = obj_Plan;
     }
 
     public void agregar_persona() {
@@ -67,26 +67,21 @@ public class Archivos_Plan {
                 file.createNewFile();
             }
 
-            //True parámetro, permite que al archivo se le pueda adicionar información 
+            
             FileWriter fw = new FileWriter(file, true);
 
             BufferedWriter bw = new BufferedWriter(fw);
 
-            bw.write(this.obj_Cliente.getNom_Cliente() + ","
-                    + this.obj_Cliente.getCedula() + ","
-                    + this.obj_Cliente.getPrimer_Apellido()
-                    + "," + this.obj_Cliente.getSegundo_Apellido() + ","
-                    + this.obj_Cliente.getNum_contrato() + ","
-                    + this.obj_Cliente.getServicio() + ","
-                    + this.obj_Cliente.getNom_Barrio() + ",");
+            bw.write(this.obj_Plan.getNombre() + ","
+                    + this.obj_Plan.getMegas() + ","
+                    + this.obj_Plan.getSimetrico()+ "," );
 
-            //Adiciona la información en el archivo 
             bw.flush();
             bw.newLine();
 
             bw.close();
 
-            JOptionPane.showMessageDialog(null, "El Cliente fue Ingresad(a) "
+            JOptionPane.showMessageDialog(null, "El Plan fue Ingresado "
                     + "Correctamente en el archivo!!!");
 
         } catch (Exception ex) {
@@ -107,7 +102,7 @@ public class Archivos_Plan {
 
             if (!file.exists()) {
                 file.createNewFile();
-                JOptionPane.showMessageDialog(null, "No hay registros de Clientes!!!");
+                JOptionPane.showMessageDialog(null, "No hay registros de Planes!!!");
 
                 return;
             }
@@ -118,10 +113,9 @@ public class Archivos_Plan {
             String lectura = "";
             int contador = 0;
 
-            lectura = "Lista de informacion de los clientes\n"
-                    + "Detalle en el siguiente orden (Nombre del cliente,"
-                    + "Cédula, Primer pellido,Segundo Apellido, Num_contrato ,"
-                    + "Servicio, Nombre del barrio. )\n\n";
+            lectura = "Lista de informacion de los Planes\n"
+                    + "Detalle en el siguiente orden (Nombre del Plan,"
+                    + "Megas, Simetrico. )\n\n";
 
             while ((registro = br.readLine()) != null) {
 
@@ -129,9 +123,8 @@ public class Archivos_Plan {
                 
                 StringTokenizer st = new StringTokenizer(registro, ",");
 
-                lectura = lectura + st.nextToken() + " " + st.nextToken() + " " + st.nextToken() + " "
-                        + " " + st.nextToken() + " " + st.nextToken() + " " + st.nextToken() + " "
-                        + " " + st.nextToken() + "\n";
+                lectura = lectura + st.nextToken() + " " + st.nextToken() + " "
+                        + st.nextToken() + "\n";
 
             }
 
@@ -140,7 +133,7 @@ public class Archivos_Plan {
             if (contador > 0) {
                 JOptionPane.showMessageDialog(null, lectura);
             } else {
-                JOptionPane.showMessageDialog(null, "No hay Clientes en el archivo");
+                JOptionPane.showMessageDialog(null, "No hay Planes en el archivo");
             }
 
         } catch (Exception ex) {
@@ -150,107 +143,32 @@ public class Archivos_Plan {
 
     }
 
-   
-    public void buscar_Cliente_por_Num_Cedula() {
-
-        try {
-
-            String registro;
-            String Nom_Cliente, Cedula, Primer_Apellido, Segundo_Apellido, Num_contrato, Servicio, Nom_Barrio, ced;
-
-            File file = new File(this.ruta + this.nombre_archivo);
-
-            
-            if (!file.exists()) {
-                file.createNewFile();
-                JOptionPane.showMessageDialog(null, "No hay registros del "
-                        + "Cliente!!!");
-                
-                return;
-            }
-
-            FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-
-            String lectura = "";
-            int contador = 0;
-
-            lectura = "INFORMACIÓN DEL CLIENTE\n"
-                    + "Detalle en el siguiente orden (Nombre del cliente , "
-                    + " Cedula, Primer apellido,Segundo apellido, Num_contrato"
-                    + "Servicio, Nom_Barrio )\n\n";
-
-            ced = JOptionPane.showInputDialog(null, "Digite el numero de "
-                    + "cedula del cliente al cual desea encontrar");
-
-            while ((registro = br.readLine()) != null) {
-
-                StringTokenizer st = new StringTokenizer(registro, ",");
-
-                Nom_Cliente = st.nextToken();
-                Cedula = st.nextToken();
-                Primer_Apellido = st.nextToken();
-                Segundo_Apellido = st.nextToken();
-                Num_contrato = st.nextToken();
-                Servicio = st.nextToken();
-                Nom_Barrio = st.nextToken();
-
-                //Compara una cédula con otra para verificar si son iguales
-                if (Cedula.equals(ced)) {
-                    contador++;
-                    lectura = lectura + Nom_Cliente + " " + Cedula + " " + Primer_Apellido + " "
-                            + " " + Segundo_Apellido + " " + Num_contrato + " " + Servicio + " "
-                            + " " + Nom_Barrio + "\n";
-
-                }
-            }
-
-            br.close();
-
-            if (contador > 0) {
-                JOptionPane.showMessageDialog(null, lectura);
-            } else {
-                JOptionPane.showMessageDialog(null, "El Cliente no fue "
-                        + "encontrado en el Sistama!!!");
-            }
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-
-        }
-
-    }
 
     public void borrar_por_Num_contrato() {
 
         try {
 
             String registro;
-            String Nom_Cliente, Cedula, Primer_Apellido, Segundo_Apellido,
-                   Num_contrato, Servicio, Nom_Barrio, primer_apellido, contra;
+            String nombre, megas, simetrico, nombreP;
             int contador = 0;
 
-            File tempDB = new File(this.ruta + "Cliente_temporal.txt");
+            File tempDB = new File(this.ruta + "Plan_temporal.txt");
             File db = new File(this.ruta + this.nombre_archivo);
 
             BufferedReader br = new BufferedReader(new FileReader(db));
             BufferedWriter bw = new BufferedWriter(new FileWriter(tempDB));
 
-            contra = JOptionPane.showInputDialog(null, "Digite "
-                    + "el número de contrato del cliente al cuál desea eliminar");
+            nombreP = JOptionPane.showInputDialog(null, "Digite "
+                    + "el nombre del plan que desea eliminar");
 
             while ((registro = br.readLine()) != null) {
 
                 StringTokenizer st = new StringTokenizer(registro, ",");
 
-                Nom_Cliente = st.nextToken();
-                Cedula = st.nextToken();
-                Primer_Apellido = st.nextToken();
-                Segundo_Apellido = st.nextToken();
-                Num_contrato = st.nextToken();
-                Servicio = st.nextToken();
-                Nom_Barrio = st.nextToken();
-                if (!Num_contrato.equals(contra)) {
+                nombre = st.nextToken();
+                megas = st.nextToken();
+                simetrico = st.nextToken();
+                if (!nombre.equals(nombreP)) {
                     bw.write(registro);
                     bw.flush();
                     bw.newLine();
@@ -269,9 +187,9 @@ public class Archivos_Plan {
             tempDB.renameTo(db);
 
             if (contador > 0) {
-                JOptionPane.showMessageDialog(null, "Cliente eliminado correctamente");
+                JOptionPane.showMessageDialog(null, "Plan eliminado correctamente");
             } else {
-                JOptionPane.showMessageDialog(null, "Cliente no fue encontrado");
+                JOptionPane.showMessageDialog(null, "Plan no fue encontrado");
             }
 
         } catch (Exception ex) {
@@ -285,41 +203,34 @@ public class Archivos_Plan {
         try {
 
             
-            String nuevoNombre = "", nuevoPrimerApellido = "", nuevoSegundoApellido = "",
-                    nuevoSintoma = "", nuevoNom_Barrio = "", nuevoDiagnostico = "", nuevaCedula = "", registro, ced, registro2;
-            String Nom_Cliente, Cedula, Primer_Apellido, Segundo_Apellido, Num_contrato, Servicio, Nom_Barrio;
-            int nuevaEdad = 0;
+            String nuevoNombre = "", nuevoMega = "", nuevoSimetrico = "", 
+                     registro, nom, registro2;
+            String nombre, megas, simetrico, Segundo_Apellido, Num_contrato, Servicio, Nom_Barrio;
+            
             int contador = 0;
 
             File db = new File(this.ruta + this.nombre_archivo);
             BufferedReader br = new BufferedReader(new FileReader(db));
 
-            File tempDB = new File(this.ruta + "Clientes_temporal.txt");
+            File tempDB = new File(this.ruta + "Plan_temporal.txt");
             BufferedWriter bw = new BufferedWriter(new FileWriter(tempDB));
 
-            ced = JOptionPane.showInputDialog(null, "Digite el número de cédula del cliente el cual desea modificar");
+            nom = JOptionPane.showInputDialog(null, "Digite el nombre del plan el cual desea modificar: ");
 
             while ((registro = br.readLine()) != null) {
 
                 StringTokenizer st = new StringTokenizer(registro, ",");
-                Nom_Cliente = st.nextToken();
-                Cedula = st.nextToken();
-                Primer_Apellido = st.nextToken();
-                Segundo_Apellido = st.nextToken();
-                Num_contrato = st.nextToken();
-                Servicio = st.nextToken();
-                Nom_Barrio = st.nextToken();
+                nombre = st.nextToken();
+                megas = st.nextToken();
+                simetrico = st.nextToken();
+                
 
-                if (ced.equals(Cedula)) {
+                if (nom.equals(nombre)) {
                     contador++;
-                    JOptionPane.showMessageDialog(null, "Los datos del cliente son: \n"
-                            + "Nombre del paciente: " + Nom_Cliente + "\n"
-                            + "La cedula del cliente: " + Cedula + "\n"
-                            + "Primer Apellido: " + Primer_Apellido + "\n"
-                            + "Segundo Apellido: " + Segundo_Apellido + "\n"
-                            + "Num_contrato: " + Num_contrato + "\n"
-                            + "Servicio: " + Servicio + "\n"
-                            + "Nom_Barrio: " + Nom_Barrio + "\n");
+                    JOptionPane.showMessageDialog(null, "Los datos del Plan son: \n"
+                            + "Nombre del Plan: " + nombre + "\n"
+                            + "Cantidad de megas: " + megas + "\n"
+                            + "Simetrico: " + simetrico + "\n");
                 }
 
             }
@@ -329,29 +240,21 @@ public class Archivos_Plan {
             if (contador == 1) {
 
                 nuevoNombre = JOptionPane.showInputDialog(null, "Digite el nuevo nombre del cliente");
-                nuevaCedula = JOptionPane.showInputDialog(null, "Digite el nueva cedula del cliente");
-                nuevoPrimerApellido = JOptionPane.showInputDialog(null, "Digite el nuevo primer apellido del cliente");
-                nuevoSegundoApellido = JOptionPane.showInputDialog(null, "Digite el nuevo segundo apellido del cliente");
-                String nuevo_Nom_Contrato = JOptionPane.showInputDialog(null, "Digite el nuevo numero de contrato del cliente");
-                String nuevaServicio = JOptionPane.showInputDialog(null, "Digite el nombre del nuevo servicio que desea adquirir");
-                nuevoNom_Barrio = JOptionPane.showInputDialog(null, "Digite el nombre del barrio");
+                nuevoMega = JOptionPane.showInputDialog(null, "Digite el nueva cedula del cliente");
+                nuevoSimetrico = JOptionPane.showInputDialog(null, "Digite el nuevo primer apellido del cliente");
+               
 
                 BufferedReader br2 = new BufferedReader(new FileReader(db));
 
                 while ((registro2 = br2.readLine()) != null) {
 
                     StringTokenizer st2 = new StringTokenizer(registro2, ",");
-                    Nom_Cliente = st2.nextToken();
-                    Cedula = st2.nextToken();
-                    Primer_Apellido = st2.nextToken();
-                    Segundo_Apellido = st2.nextToken();
-                    Num_contrato = st2.nextToken();
-                    Servicio = st2.nextToken();
-                    Nom_Barrio = st2.nextToken();
-                    if (ced.equals(Cedula)) {
-                        bw.write(nuevoNombre + "," + nuevaCedula + ","
-                                + nuevoPrimerApellido + "," + nuevoSegundoApellido + "," + nuevo_Nom_Contrato
-                                + "," + nuevaServicio + "," + nuevoNom_Barrio);
+                    nombre = st2.nextToken();
+                    megas = st2.nextToken();
+                    simetrico = st2.nextToken();
+                    if (nom.equals(nombre)) {
+                        bw.write(nuevoNombre + "," + nuevoNombre + ","
+                                + nuevoMega + "," + nuevoSimetrico);
                     } else {
                         bw.write(registro2);
                     }
@@ -364,12 +267,12 @@ public class Archivos_Plan {
                 db.delete();
                 tempDB.renameTo(db);
 
-                JOptionPane.showMessageDialog(null, "El cliente "
-                        + "fue actualizadao correctamente");
+                JOptionPane.showMessageDialog(null, "El Plan "
+                        + "fue actualizado correctamente!!!");
 
             } else {
-                JOptionPane.showMessageDialog(null, "La cédula del cliente "
-                        + "no fue encontrada en el sistema");
+                JOptionPane.showMessageDialog(null, "El nombre del cliente "
+                        + "no fue encontrada en el sistema!!!");
             }
 
         } catch (Exception ex) {
