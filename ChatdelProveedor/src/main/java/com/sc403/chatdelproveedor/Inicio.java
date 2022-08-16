33f6f35a8192dfe23a359b169603f08ce40d4883
Sean17bcr <seanbcr123@gmail.com>
2022-08-15 18:57:44 -0600

    Chat proveedor deleted

--- ChatdelProveedor/src/main/java/com/sc403/chatdelproveedor/Inicio.java
+++
@@ -1,9 +0,0 @@
-package com.sc403.chatdelproveedor;
-public class Inicio {
-    public static void main(String[] args) {
-        ProveedorChat proveChat = new ProveedorChat();
-        proveChat.setVisible(true);
-        proveChat.setLocationRelativeTo(null);
-    }
-    //Hola
-}
